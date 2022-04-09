a7137a3d0d09927762423ad5853bd3498eef6132
jaehyeon <jaehyeon0825@gmail.com>
2022-04-09 21:40:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/schoolbusapp/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.schoolbusapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val DURATION: Long = 1500
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, DURATION)
+    }
+}
