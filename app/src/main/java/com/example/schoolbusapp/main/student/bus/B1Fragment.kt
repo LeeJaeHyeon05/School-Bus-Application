f1107d48d2b0df16471174751183363345ee391a
JaeHyeon <jaehyeon0825@gmail.com>
2022-04-17 09:34:39 +0700

    add firebase

--- app/src/main/java/com/example/schoolbusapp/main/student/bus/B1Fragment.kt
+++ app/src/main/java/com/example/schoolbusapp/main/student/bus/B1Fragment.kt
@@ -1,22 +1,50 @@
 package com.example.schoolbusapp.main.student.bus
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.schoolbusapp.R
+import com.example.schoolbusapp.databinding.FragmentB1Binding
+import com.example.schoolbusapp.databinding.FragmentTeacherHomeBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class B1Fragment : Fragment() {
 
+    private var mBinding: FragmentB1Binding? = null
+    private val binding get() = mBinding!!
+
+    private val database = FirebaseDatabase.getInstance()
+    private val myRef = database.getReference("bus")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b1, container, false)
+        mBinding = FragmentB1Binding.inflate(inflater, container, false)
+
+        val b1Text = binding.Test
+
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.child("start")
+                b1Text.text = "$value"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("error", "실패")
+            }
+        })
+
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/schoolbusapp/main/teacher/bus/B1Activity.kt
+++ app/src/main/java/com/example/schoolbusapp/main/teacher/bus/B1Activity.kt
@@ -2,20 +2,36 @@ package com.example.schoolbusapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 
 class B1Activity : AppCompatActivity() {
 
+    private val database = FirebaseDatabase.getInstance()
+    private val myRef = database.getReference("bus")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b1)
         backBtn()
 
+        val csb1 = findViewById<Button>(R.id.cantavilStartButton)
+        csb1.setOnClickListener {
+            val b1 = myRef.child("start")
+            b1.setValue(true)
+
+        }
+
 
 
     }
-
     private fun backBtn() {
         val backBtn = findViewById<Button>(R.id.backPressedButton)
         backBtn.setOnClickListener {
