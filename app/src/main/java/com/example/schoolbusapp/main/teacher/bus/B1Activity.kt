776258f5d1303b657e521b80e4920545ed832fe8
jaehyeon <jaehyeon0825@gmail.com>
2022-04-27 20:40:40 +0700

    firebase

--- app/src/main/java/com/example/schoolbusapp/main/teacher/bus/B1Activity.kt
+++ app/src/main/java/com/example/schoolbusapp/main/teacher/bus/B1Activity.kt
@@ -1,8 +1,12 @@
 package com.example.schoolbusapp
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class B1Activity : AppCompatActivity() {
 
@@ -11,6 +15,28 @@ class B1Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b1)
         backBtn()
+
+        val db = Firebase.firestore
+
+
+        val cantavilStartButton = findViewById<Button>(R.id.cantavilStartButton)
+        val ParkStartButton = findViewById<Button>(R.id.ParkStartButton)
+        val LandmarkStartButton = findViewById<Button>(R.id.LandmarkStartButton)
+        val landmarkPlusStartButton = findViewById<Button>(R.id.landmarkPlusStartButton)
+        val landmark5StartButton = findViewById<Button>(R.id.landmark5StartButton)
+
+        cantavilStartButton.setOnClickListener {
+            val bus1Info = db.collection("busInfo").document("b1")
+            bus1Info.update("cantavil", true)
+                .addOnSuccessListener {
+                    Log.d(TAG, "성공")
+                }
+                .addOnCanceledListener {
+                    Log.d(TAG, "실패")
+                }
+        }
+
+
 
 
 
